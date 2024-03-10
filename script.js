9a596eddba9704a77fb1dbc72a438b51f44b4e14
salmankhanmsk786 <salman.khan.396@my.csun.edu>
2024-03-09 16:23:15 -0800

    Update profile section layout and text

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+
+const script = document.createElement("script");
+script.src = "https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js";
+document.head.appendChild(script);
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  script.onload = function() {
+    // Initialize Typed.js here
+    const typed = new Typed('.section__text__p2', {
+      strings: ["Full-Stack Developer", "Web Developer", "App Developer "],
+      typeSpeed: 60,
+      backSpeed: 50,
+      backdelay : 2000,
+      loop: true
+
+    });
+  };
+  document.head.appendChild(script);
+  
+
