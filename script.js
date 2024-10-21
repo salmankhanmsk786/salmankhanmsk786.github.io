43e2e7ad9b074f29220a7232d51f95e1fa9a8a77
salmankhanmsk786 <salman.khan.396@my.csun.edu>
2024-10-21 14:35:14 -0700

    Refactor project descriptions for multimedia and Minesweeper game

--- script.js
+++ script.js
@@ -22,5 +22,87 @@ script.onload = function() {
 };
 
   document.head.appendChild(script);
+
+// Particles.js - NASA-Like Effect with Light Background and Dark Particles
+particlesJS("particles-js", {
+  "particles": {
+      "number": {
+          "value": 100, // Number of particles on the screen
+          "density": {
+              "enable": true,
+              "value_area": 800
+          }
+      },
+      "color": {
+          "value": "#ADD8E6" // light blue color for the particles
+      },
+      "shape": {
+          "type": "circle", // Only circles
+          "stroke": {
+              "width": 0,
+              "color": "#000000"
+          }
+      },
+      "opacity": {
+          "value": 1, // Slight opacity for better visibility
+          "random": true,
+          "anim": {
+              "enable": true,
+              "speed": 1,
+              "opacity_min": 0.3,
+              "sync": false
+          }
+      },
+      "size": {
+          "value": 7, // Medium particle size for better contrast
+          "random": true,
+          "anim": {
+              "enable": true,
+              "speed": 2,
+              "size_min": 0.5,
+              "sync": false
+          }
+      },
+      "line_linked": {
+          "enable": false // No connecting lines
+      },
+      "move": {
+          "enable": true,
+          "speed": 0.5, // Slow floating effect
+          "direction": "none",
+          "random": true,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false
+      }
+  },
+  "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+          "onhover": {
+              "enable": true,
+              "mode": "bubble" // Particles expand on hover
+          },
+          "onclick": {
+              "enable": true,
+              "mode": "push" // Adds new particles on click
+          }
+      },
+      "modes": {
+          "bubble": {
+              "distance": 200,
+              "size": 8, // Bubble effect size
+              "duration": 2,
+              "opacity": 0.8,
+              "speed": 3
+          },
+          "push": {
+              "particles_nb": 4 // Adds 4 particles on click
+          }
+      }
+  },
+  "retina_detect": true
+});
+
   
 
